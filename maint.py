20c996405e1ed96d99bde41f826ac92be41e9c38
chenhowie <chenmax.ibm@gmail.com>
2024-03-09 14:52:04 +0800

    test

---
+++ maint.py
@@ -0,0 +1,35 @@
+import core.main_window.main_window as func1
+import core.data_process.feature_engineering.feature_extract as func2
+import core.model.train_model as func3
+import core.model.model_insightes as func4
+
+if __name__ == '__main__':
+    path = "core/demo/data.csv"
+
+
+    # #取得參數 (回傳參數)
+    # print(f"features : {func1.load_feature(path)}")
+
+    # 資料處裡-字串處理 (回傳位置)
+    print(func2.string_split(path, 'f1', "-"))
+
+    # 資料處裡-分群 (回傳位置)
+    # print(func2.cluster(path, 'f2', 3))
+
+    # 建立模型
+    print(func3.train_model(training_features="core/demo/temp.csv", training_target="core/demo/target.csv", model_name="test", model="Decision_Tree"))
+
+    # 模型列表
+    # print(f"model list : {func1.list_model()}")
+
+    # # 使用模型預測新資料
+    print(func3.predict_model("core/demo/temp.csv", "test"))
+
+    # # 使用 permutation importance 分析模型 (回傳.csv檔路徑)
+    print(func4.perimp("test", "core/demo/temp.csv", "core/demo/target.csv"))
+
+    # # Partial Dependence display 1D
+    print(func4.pdpd1d("test", "core/demo/temp.csv", "core/demo/target.csv"))
+
+    # # Partial Dependence display 2D
+    print(func4.pdpd2d("test", "core/demo/temp.csv", "core/demo/target.csv", "f2", "f3"))
