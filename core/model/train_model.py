18569f1c6db86d68d080d2bdb6341af1589f1940
PringDeSu <ianneeddiamond@gmail.com>
2024-03-11 14:38:08 +0800

    Pring
    2024-03-11
    
    Hacked by Howie

--- core/model/train_model.py
+++ core/model/train_model.py
@@ -13,12 +13,16 @@ def train_model(training_features, training_target, model_name, model="Decision_
         # feature_min_ = feature.min(axis=0)
         # target_max_ = target.max(axis=0)
         # target_min_ = target.min(axis=0)
-        # X_train = feature.sample(frac=0.7)
-        # X_valid = feature.drop(X_train.index)
-        # Y_valid = target.drop(X_train.index)
-        # Y_train = target.drop(Y_valid.index)
-        X_train = feature
-        Y_train = target
+        X_train = feature.sample(frac=0.7)
+        X_train = X_train.sort_index()
+        X_valid = feature.drop(X_train.index)
+        X_valid = X_valid.sort_index()
+        Y_valid = target.drop(X_train.index)
+        Y_valid = Y_valid.sort_index()
+        Y_train = target.drop(Y_valid.index)
+        Y_train = Y_train.sort_index()
+        # X_train = feature
+        # Y_train = target
         # X_train = (X_train - feature_min_) / (feature_max_ - feature_min_)
         # X_valid = (X_valid - feature_min_) / (feature_max_ - feature_min_)
         # Y_train = (Y_train - target_min_) / (target_max_ - target_min_)
@@ -27,9 +31,9 @@ def train_model(training_features, training_target, model_name, model="Decision_
         model = DecisionTreeRegressor()
         model.fit(X_train, Y_train)
         from sklearn.metrics import mean_absolute_error
-        predicted = model.predict(X_train)
+        predicted = model.predict(X_valid)
         pickle.dump(model, open(f"data/model/{model_name}", 'wb'))
-        return f"success! mean absolute error : {mean_absolute_error(Y_train, predicted)}"
+        return f"success! mean absolute error : {mean_absolute_error(Y_valid, predicted)}"
     else:
         return ValueError(f"Model not found : \"{model_name}\"")
     
